999001bf6a788a8bf1479274a94886f3e90ba73d
Soft-Serve <soft-serve@localhost>
2025-04-16 21:50:42 +0200

    fix: ssh key source

--- .github/hack/soft-serve/start.sh
+++ .github/hack/soft-serve/start.sh
@@ -13,39 +13,39 @@ log() {
 }
 
 # cleanup
-rm -rf $BASE/hack/soft-serve/ssh-key
-rm -rf $BASE/hack/soft-serve/ssh-key.pub
-rm -rf $BASE/hack/soft-serve/data
-rm -rf $BASE/hack/soft-serve/gitops-test
+rm -rf $BASE/.github/hack/soft-serve/ssh-key
+rm -rf $BASE/.github/hack/soft-serve/ssh-key.pub
+rm -rf $BASE/.github/hack/soft-serve/data
+rm -rf $BASE/.github/hack/soft-serve/gitops-test
 
 remove_ssh_key() {
-  ssh-add -d $BASE/hack/soft-serve/ssh-key.pub  
+  ssh-add -d $BASE/.github/hack/soft-serve/ssh-key.pub  
 }
 
 trap remove_ssh_key INT
 
 # generate ssh key
-ssh-keygen -t ed25519 -N '' -f $BASE/hack/soft-serve/ssh-key
+ssh-keygen -t ed25519 -N '' -f $BASE/.github/hack/soft-serve/ssh-key
 
 # set config vars
-export SOFT_SERVE_INITIAL_ADMIN_KEYS=$(cat $BASE/hack/soft-serve/ssh-key.pub)
+export SOFT_SERVE_INITIAL_ADMIN_KEYS=$(cat $BASE/.github/hack/soft-serve/ssh-key.pub)
 
-mkdir -p $BASE/hack/soft-serve/data
+mkdir -p $BASE/.github/hack/soft-serve/data
 
-docker compose -f $BASE/hack/soft-serve/docker-compose.yml up -d
+docker compose -f $BASE/.github/hack/soft-serve/docker-compose.yml up -d
 
 sleep 1
 
-ssh-add $BASE/hack/soft-serve/ssh-key
+ssh-add $BASE/.github/hack/soft-serve/ssh-key
 echo "Creating repo"
-ssh -p 23231 -o StrictHostKeychecking=no -i $BASE/hack/soft-serve/ssh-key localhost repo create gitops-test
+ssh -p 23231 -o StrictHostKeychecking=no -i $BASE/.github/hack/soft-serve/ssh-key localhost repo create gitops-test
 
 echo "Cloning repo"
-export GIT_SSH_COMMAND="ssh -o StrictHostKeychecking=no -i $BASE/hack/soft-serve/ssh-key"
-git clone ssh://localhost:23231/gitops-test.git $BASE/hack/soft-serve/gitops-test
-mkdir -p $BASE/hack/soft-serve/gitops-test/applications/dev/service-test
-cp $BASE/hack/soft-serve/fixtures/values.yaml $BASE/hack/soft-serve/gitops-test/applications/dev/service-test/values.yaml
-cd $BASE/hack/soft-serve/gitops-test
+export GIT_SSH_COMMAND="ssh -o StrictHostKeychecking=no -i $BASE/.github/hack/soft-serve/ssh-key"
+git clone ssh://localhost:23231/gitops-test.git $BASE/.github/hack/soft-serve/gitops-test
+mkdir -p $BASE/.github/hack/soft-serve/gitops-test/applications/dev/service-test
+cp $BASE/.github/hack/soft-serve/fixtures/values.yaml $BASE/.github/hack/soft-serve/gitops-test/applications/dev/service-test/values.yaml
+cd $BASE/.github/hack/soft-serve/gitops-test
 git config user.name "Soft-Serve"
 git config user.email "soft-serve@localhost"
 git checkout -b main
