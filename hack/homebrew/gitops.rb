7df6e7f1264ba3d3fc7d3d6173241d219cdad334
Max Partenfelder <max.partenfelder@wilde-it.com>
2025-04-17 19:50:58 +0200

    fix: setting release version in golang homebrew golang build

--- hack/homebrew/gitops.rb
+++ hack/homebrew/gitops.rb
@@ -10,7 +10,7 @@ class Gitops < Formula
     # ENV["GOPATH"] = buildpath
     path = buildpath/"cmd/gitops"
     cd path do
-      system "go", "build", "-o", "#{bin}/gitops"
+      system "go", "build", "-ldflags=\"-s -w -X 'main.version=${RELEASE_VERSION}'\"" "-o", "#{bin}/gitops"
     end
   end
 
